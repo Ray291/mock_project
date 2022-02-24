6b83a53e9ba3268213abc3d0ead65ae661a88120
D.Espresso <manhpd.esp@gmail.com>
2022-02-24 16:32:23 +0700

    Update

--- Connect_UpdateSQL_Template.py
+++ Connect_UpdateSQL_Template.py
@@ -9,7 +9,7 @@ from schema import *
 import mysql.connector
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
-
+from datetime import timedelta
 
 ######### Connect To DATABASE ################
 mydb = mysql.connector.connect(
@@ -50,7 +50,7 @@ from schema import *
 
 
 ### Read Write to dataframe ###############
-Store_Date =  '2021-11-01'
+Store_Date =  '2021-11-03'
 
 def read_enity(entity, schema, Store_Date, delimiter='\t', header='True'):
         return spark.read.format("csv").schema(schema).options(header=header, delimiter=delimiter) \
@@ -63,12 +63,11 @@ except Exception as e:
      print("ERROR:",e)       
 user = (user
        .withColumn("updatedTime", date_trunc("second", col("updatedTime")))
-        .withColumn("birthdate", to_date(col("birthdate")))
-        .withColumn("Age", floor(datediff(current_date(), col("birthdate"))/365.25)))
+        .withColumn("birthdate", to_date(col("birthdate"))) )
 
 w = Window.partitionBy(user.userid).orderBy(user.updatedTime.desc())
 user_info = user.withColumn("row_number", row_number().over(w)).filter(col("row_number")==1).drop("row_number")
-user_info.show(5)
+user_info.orderBy("userid").show(10)
 # user_info.write.mode("overwrite").jdbc(url=url,table="user_info",properties=properties)
 
 
@@ -83,43 +82,43 @@ def config_update(row ):
     ProfileLevel = row.__getitem__("profileLevel")
     gender = row.__getitem__("gender")
     updatedTime =  row.__getitem__("updatedTime")
-    Age = row.__getitem__("Age")
 
     ## Check in DataFrame if record exists
     sql_query = f"SELECT * FROM user_info where userid = '{user_id}' "
     mycursor.execute(sql_query)
     result = mycursor.fetchall()
     number_sameID = len(result)
-    date_in_data_in_DB = result[0][4]
-    val = (user_id,birthdate,ProfileLevel,gender,updatedTime,Age)
-    val2=(birthdate,ProfileLevel,gender,updatedTime,Age,user_id)
-    
-    if number_sameID > 0:
-        option = "UPDATE"
-        print("DB",date_in_data_in_DB)
-        print("root",updatedTime)
-        diff_time =  updatedTime - date_in_data_in_DB 
+    val = (user_id,birthdate,ProfileLevel,gender,updatedTime)
+    val2=(birthdate,ProfileLevel,gender,updatedTime,user_id)
 
-        if diff_time >= 0 :
+    if number_sameID > 0:
+        date_in_data_in_DB = result[0][4]
+        option = "UPDATE"
+        print("[ DB:",date_in_data_in_DB,"|","root:",updatedTime,"]")
+        diff_time =  updatedTime - date_in_data_in_DB 
+        print(diff_time)
+        if diff_time <= timedelta(days = 0, hours=0,minutes=0,seconds =0 ) :
             pass
         else:
-            print("Update Data")
+            print("Update ID",user_id)
             query = f"""
-            {option} user_info SET birthdate=%s , ProfileLevel=%s , gender=%s ,updatedTime =%s,Age=%s
+            {option} user_info SET birthdate=%s , ProfileLevel=%s , gender=%s ,updatedTime =%s
             WHERE userid = %s
             """
             mycursor.execute(query,val2)
+            mydb.commit()
 
     else:
         option = "INSERT"
         query = f""" 
-        {option} INTO user_info(userid,birthdate,profileLevel,gender,updatedTime,Age) VALUES (%s,%s,%s,%s,%s,%s) """
+        {option} INTO user_info(userid,birthdate,profileLevel,gender,updatedTime) VALUES (%s,%s,%s,%s,%s) """
         mycursor.execute(query,val)
+        mydb.commit()
+        print("INSERT NEW ID ",user_id)
 
     
 for row in list_row:
     config_update(row)
-    break
 
 
 
